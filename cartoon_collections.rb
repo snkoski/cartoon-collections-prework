456ce3e6264999f48b964e4f9d316f67c60a6e79
Shawn Koski <shawnnkoski@gmail.com>
2018-04-01 17:02:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(planeteers)# code an argument here
   # Your code here
   planeteers.map! do |planeteer|
-    planeteer.capitalize += "!"
+    planeteer.capitalize + "!"
   end
 end
 
